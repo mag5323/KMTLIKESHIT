02009b8fd89286928fcdd12790e01965fa13d8f6
mag5323 <mag5323@gmail.com>
2015-02-02 18:02:08 +0800

    get total price of every column

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ d3.json("data/2014_estate.json")
     };
 
     delete estate['救國團劍潭青年活動中心建物一覽表'];
+
+    for (key in estate) {
+      var kmt = estate[key].data;
+      var priceTmp = _.pluck(kmt, '2014年市值推估');
+      var price = _.pluck(priceTmp, '總價（元）');
+      var sum = _.reduce(price, function(memo, num){ return memo + num; }, 0);
+      data.content.push({label: key, value: sum});
+    }
   });
